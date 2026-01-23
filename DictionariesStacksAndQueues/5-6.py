72c4a1a50051afb96c20212f3c6d07b049d8d05e
damianjuszczak <244980@student.uek.krakow.pl>
2026-01-23 18:13:29 +0100

    DictonariesStackandQueues

--- DictionariesStacksAndQueues/5-6.py
+++ DictionariesStacksAndQueues/5-6.py
@@ -9,12 +9,6 @@ advanced_data = {
    "interest": ["reading", "swimming"]
 }
 
-# Method 1: Using the union operator (|) - available in Python 3.9+
 person = basic_data | advanced_data
 
-# Method 2: Using the update() method (alternative)
-# person = basic_data.copy()
-# person.update(advanced_data)
-
-# Print the contents of the 'person' dictionary
 print(person)
